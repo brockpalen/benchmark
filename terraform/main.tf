c6ff8f84ccd742f4c6a7083f617aaf6eb4c64e6e
Brock Palen <brockp@mlds-networks.com>
2018-01-15 16:09:41 -0500

    changed google config for more cpu types

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,36 @@
 ################################################
 #  Google Cloud options
 
-module "gcp" {
+## Google standard Sandy Bridge
+module "gcp-sandybridge" {
   source          = "./gcp"
   gcp_hostname    = "gcp-highcpu"
   gcp_cred        = "terraform-admin.json"
   gcp_count       = 1
   gcp_nodetype    = "n1-highcpu-32"
+  gcp_min_cpu_platform = "Intel Sandy Bridge"
   gcp_user        = "brockp"
   gcp_ssh_pub_key = "/home/brockp/.ssh/id_rsa.pub"
 }
 
-output "gcp_highcpu_ips" {
-  value = "${module.gcp.gcp_public_ips}"
+output "gcp_sandybridge_ips" {
+  value = "${module.gcp-sandybridge.gcp_public_ips}"
 }
 
-## Google Cloud standard CPU version
-module "gcp-standard" {
+## Google Cloud broadwell CPU version
+module "gcp-broadwell" {
   source          = "./gcp"
-  gcp_hostname    = "gcp-standard"
+  gcp_hostname    = "gcp-broadwell"
   gcp_cred        = "terraform-admin.json"
   gcp_count       = 1
-  gcp_nodetype    = "n1-standard-32"
+  gcp_nodetype    = "n1-highcpu-32"
+  gcp_min_cpu_platform = "Intel Broadwell"
   gcp_user        = "brockp"
   gcp_ssh_pub_key = "/home/brockp/.ssh/id_rsa.pub"
 }
 
-output "gcp_standard_ips" {
-  value = "${module.gcp-standard.gcp_public_ips}"
+output "gcp_broadwell_ips" {
+  value = "${module.gcp-broadwell.gcp_public_ips}"
 }
 
 ## Google Cloud skylake CPUs version
